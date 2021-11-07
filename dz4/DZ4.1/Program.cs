4d6971547775841f05aacaffe838873a87b20da0
FreLoy <Lokkis10@yandex.ru>
2021-11-07 13:04:11 +0300

    Сделал ДЗ№4
    вот они снизу вверх <3

---
+++ dz4/DZ4.1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DZ4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat;
+
+            Console.WriteLine("Введите размер матрицы: N =");
+            int N = int.Parse(Console.ReadLine());
+            mat = new int[N, N];
+            Random rand = new Random();
+
+            //Заполняем матрицу
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = rand.Next(-10, 10);
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("{0}\t", mat[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            //Перемножаем только положительные значения в предобъявленную переменную
+            int[] RowTotal = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] > 0)
+                    {
+                        if (!RowTotal[i].Equals(0))
+                            RowTotal[i] *= mat[i, j];
+                        else RowTotal[i] = mat[i, j];
+
+                    }
+                }
+
+            }
+            for (int i = 0; i < N; i++)
+                Console.WriteLine("Строка  {0} матрицы имеет произведение положительных элементов равное: {1}", i + 1, RowTotal[i]);
+            Console.ReadKey(true);
+
+            int[] SUS = new int[2*N-1];
+            int chetchik = 0;
+            for (int i = 0; i < N; i++) for(int j = 0; j < N; j++)
+            {
+                if (i == j)
+                {
+                        SUS[0] += mat[i, j];
+                }
+                else if( j<=i-1)
+                    {
+                        SUS[i - j ] += mat[i, j];
+                    }
+                else if (i<=j-1)
+                    {
+                        SUS[j - i + N - 1] += mat[i, j];
+                    }
+            }
+            int max = SUS[0];
+            for(int i = 0; i < 2*N-1; i++)
+            {
+                if (SUS[i] > max) max = SUS[i];
+            }
+            Console.WriteLine($"Максимум среди сумм элементов диагоналей, параллельных главной диагонали матрицы:{max}");
+        }
+    }
+}
+
+
---
+++ dz4/dz4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Mass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Введите размер массива N = ");
+            int N = int.Parse(Console.ReadLine());
+            int[] Array = new int[N];
+
+            //Заполняем массив
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = rand.Next(-10, 10);
+                Console.Write("{0} ", Array[i]);
+            }
+            //Вычисляем сумму элементов массива с нечетными номерами
+            int count = 0;
+            for (int i = 0; i < N; ++i)
+            {
+                if (Array[i] % 2 != 0)
+                    count += Array[i];
+            }
+            Console.Write("\n\nCуммa элементов массива с нечетными номерами : {0}", count);
+
+            //Вычисляем сумму элементов расположенных между первым и последним отрицательными элементами
+
+            int SM = Array.SkipWhile(r => r >= 0).Skip(1).Reverse().SkipWhile(r => r >= 0).Skip(1).Sum();
+            Console.WriteLine("\nСумма элементов между отрицательными элементами = {0}", SM.ToString());
+
+            //Убираем элементы с модулем менее единицы
+            for (int i = 0; i < N; i++)
+            {
+                if (Math.Abs(Array[i]) < 1) Array[i] = 0;
+            }
+
+            //Сжимаем массив и заполняем освобожденные элементы нулями
+            var Array2 = Array.Where(n => !(Math.Abs(n) <= 1));
+            Array2 = Array2.Concat(new int[Array.Length - Array2.Count()]).ToArray();
+            Console.WriteLine("Cжатый массив:");
+            foreach (int i in Array2) Console.Write("{0} ", i);
+            Console.Read();
+        }
+
+    }
+}
